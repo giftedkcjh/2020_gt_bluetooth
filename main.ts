d1628bb7dad47f9ec9d900ab1ee4d49a344ccd40
giftedkcjh <68175417+giftedkcjh@users.noreply.github.com>
2020-07-14 17:48:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("-")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString(".")
+})
